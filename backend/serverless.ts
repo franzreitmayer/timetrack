1e5761f7d90c1b318a8f4ff0d81940f5cb707e3b
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2021-03-11 21:24:24 +0100

    inital commit

---
+++ backend/serverless.ts
@@ -0,0 +1,33 @@
+import type { AWS } from '@serverless/typescript';
+
+import hello from '@functions/hello';
+
+const serverlessConfiguration: AWS = {
+  service: 'ttapi',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    stage: "${opt:stage, 'dev'}",
+    region: 'eu-central-1',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  // import the function via paths
+  functions: { hello },
+};
+
+module.exports = serverlessConfiguration;
---
+++ client/uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "eu/reitmayer/tt/client/timeTrackingClient/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("eu.reitmayer.tt.client.timeTrackingClient.controller.MainView", {});
+});
